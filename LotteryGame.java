9d5a95ce02bbc23baabe05141f60af94d15d9425
mihaela caloianu <albumihaela87@yahoo.com>
2022-06-01 17:39:22 +0300

    Let's win!

---
+++ LotteryGame.java
@@ -0,0 +1,34 @@
+package TasksAgain;
+
+import java.util.Random;
+
+public class LotteryGame {
+    public static void main(String[] args) {
+
+        int[] ticket = new int[6];
+        int generatedNumbers = 0;
+        Random random = new Random();
+
+        while (generatedNumbers < ticket.length) {
+            int luckyNumber = random.nextInt(30) + 1;
+            boolean isUnique = true;
+
+            for (int i = 0; i < generatedNumbers; i++) {
+                if (luckyNumber == ticket[i]) {
+                    isUnique = false;
+                }
+            }
+
+            if (isUnique) {
+                ticket[generatedNumbers] = luckyNumber;
+                generatedNumbers++;
+            }
+        }
+        System.out.println("The lucky numbers are:");
+        for (int i = 0; i < ticket.length; i++) {
+            System.out.print(ticket[i] + " ");
+        }
+    }
+
+
+}
